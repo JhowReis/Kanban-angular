69f1b0b2eae9bba0795ebe9e3087489c478ded96
Jonathan Reis <jonathan.linkedin2019@gmail.com>
2021-12-07 22:26:50 -0300

    project finished

---
+++ src/app/pages/main-view/main-view.component.ts
@@ -0,0 +1,59 @@
+import { Component, OnInit } from '@angular/core';
+import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
+import { Column } from 'src/app/model/column.model';
+import { Board } from 'src/app/model/board.model';
+
+
+@Component({
+  selector: 'app-main-view',
+  templateUrl: './main-view.component.html',
+  styleUrls: ['./main-view.component.scss']
+})
+export class MainViewComponent implements OnInit {
+
+  constructor() { }
+
+  board: Board = new Board('Test Board', [
+    new Column('Ideas', [
+      "this is a new column",
+      "testing this new tasks",
+      "building something important"
+    ]),
+    new Column('development', [
+      "testing program",
+      "implementation task",
+      "building something important"
+    ]),
+    new Column('Todo',[
+      'Get to work',
+      'Pick up groceries',
+      'Go home',
+      'Fall asleep'
+    ]),
+    new Column('Done', [
+      'Get up',
+      'Brush teeth',
+      'Take a shower',
+      'Check e-mail',
+      'Walk dog'
+    ])
+  
+  
+  ])
+
+  ngOnInit(): void {
+  }
+
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(event.previousContainer.data,
+                        event.container.data,
+                        event.previousIndex,
+                        event.currentIndex);
+    }
+  }
+
+}
